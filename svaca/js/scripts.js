3af577cd7b6429b43fbb33c55d1cc1c8d5fc8cfc
zdendapa <zdenek.pavlicek@gmail.com>
2014-01-08 18:44:37 +0100

    html cely prubeh proklik nakoupeni s drobnymi chybami k ukazce

--- svaca/js/scripts.js
+++ svaca/js/scripts.js
@@ -199,7 +199,9 @@ function registrovat() {
 function nactiZboziAjax() {
     $.ajax({ url:'http://demo.livecycle.cz/fajnsvaca/api/listProducts',
         success: function(data) {
-            alert("data prisla");
+            alert("data prisla zobrazuji");
+            alert($.param(data));
+            alert("konec zobrazeni");
             if( data.status == "error" && data.code == "not logged")
             {
                 console.log(data.msg);
